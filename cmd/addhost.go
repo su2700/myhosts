d6efec488ad5eb5b036d7848f253805a262a8723
N04h <noah.h.liu@gmail.com>
2024-05-10 15:46:02 +0200

    0.0.1 version

---
+++ cmd/addhost.go
@@ -0,0 +1,53 @@
+package cmd
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/spf13/cobra"
+)
+
+// addhostCmd represents the addhost command
+var addhostCmd = &cobra.Command{
+	Use:   "addhost",
+	Short: "Add an IP and domain to /etc/hosts",
+	Long: `This command adds a new IP address and domain name to the /etc/hosts file,
+ensuring there are no conflicts with existing entries.`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		ip, _ := cmd.Flags().GetString("ip")
+		domain, _ := cmd.Flags().GetString("domain")
+		return addToHosts(ip, domain)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(addhostCmd)
+
+	// Here you will define your flags and configuration settings.
+	addhostCmd.Flags().StringP("ip", "i", "", "IP address to add")
+	addhostCmd.Flags().StringP("domain", "d", "", "Domain name to add")
+	addhostCmd.MarkFlagRequired("ip")
+	addhostCmd.MarkFlagRequired("domain")
+}
+
+func addToHosts(ip, domain string) error {
+	file, err := os.OpenFile("/etc/hosts", os.O_RDWR, 0644)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.Contains(line, ip) || strings.Contains(line, domain) {
+			return errors.New("entry already exists")
+		}
+	}
+
+	_, err = file.WriteString(fmt.Sprintf("\n%s\t%s\n", ip, domain))
+	return err
+}
